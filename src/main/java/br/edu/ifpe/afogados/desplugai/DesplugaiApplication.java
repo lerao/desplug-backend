9243615fbf9784dc3fed3ee5484db7eada517a8d
Anderson Campos <anderson.camposs87@outlook.com>
2026-09-01 19:26:16 -0300

    Commit inicial, criação da estrutura inicial do projeto

---
+++ src/main/java/br/edu/ifpe/afogados/desplugai/DesplugaiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.afogados.desplugai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesplugaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesplugaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpe/afogados/desplugai/DesplugaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.afogados.desplugai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesplugaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
