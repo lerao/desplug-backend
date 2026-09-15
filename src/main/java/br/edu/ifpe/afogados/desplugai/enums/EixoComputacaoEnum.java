9322d253073ac941ef33f18abc056fa28e29f775
Breno Rezende <154555896+Kelv03@users.noreply.github.com>
2026-09-15 19:26:57 -0300

    corrigir o enum da Habilidade da BNCC

--- src/main/java/br/edu/ifpe/afogados/desplugai/enums/EixoComputacaoEnum.java
+++ src/main/java/br/edu/ifpe/afogados/desplugai/enums/EixoComputacaoEnum.java
@@ -1,7 +1,17 @@
 package br.edu.ifpe.afogados.desplugai.enums;
 
 public enum EixoComputacaoEnum {
-    PENSAMENTO_COMPUTACIONAL,
-    MUNDO_DIGITAL,
-    CULTURA_DIGITAL
+    PENSAMENTO_COMPUTACIONAL("PENSAMENTO COMPUTACIONAL"),
+    MUNDO_DIGITAL("MUNDO DIGITAL"),
+    CULTURA_DIGITAL("CULTURA DIGITAL");
+
+    private final String descricao;
+
+    EixoComputacaoEnum(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
